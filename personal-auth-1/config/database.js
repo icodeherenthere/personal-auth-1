831e6d744d923475747af586245544af1f5d847f
Malik Campbell-Greene <126094295+icodeherenthere@users.noreply.github.com>
2023-04-29 19:01:15 -0400

    Add files via upload

---
+++ personal-auth-1/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://malikcgdev:carDatapw@cardata.hmktgae.mongodb.net/allContent?retryWrites=true&w=majority',
+    'dbName': 'allContent'
+};
